3f565a92d6297b854675665161a8389e0992285f
jessica <jessymontano@hotmail.com>
2025-04-08 21:30:50 -0700

    agregar ruta para equipos

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // IMPORTACIONES
-import express from 'express';
-import dotenv from 'dotenv';
-import conectarDB from './config/db.js';
+import express from "express";
+import dotenv from "dotenv";
+import conectarDB from "./config/db.js";
 
 // RUTAS PARA LA API
-import reactivoRoutes from './routes/reactivoRoutes.js';
+import reactivoRoutes from "./routes/reactivoRoutes.js";
+import equipoRoutes from "./routes/equipoRoutes.js";
 
 const app = express();
 app.use(express.json());
@@ -14,8 +15,9 @@ dotenv.config();
 conectarDB();
 
 app.use("/api/reactivos", reactivoRoutes);
+app.use("/api/equipos", equipoRoutes);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-    console.log(`Servidor funcionando en el puerto ${PORT}`);
+  console.log(`Servidor funcionando en el puerto ${PORT}`);
 });
