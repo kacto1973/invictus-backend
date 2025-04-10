02cda6eaa17a094a54c5ea0e4cca669410640313
jessica <jessymontano@hotmail.com>
2025-04-09 20:15:27 -0700

    agregar rutas para reserva y mantenimiento, agregar cron job para verificar estado de equipos

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ import express from "express";
 import dotenv from "dotenv";
 import conectarDB from "./config/db.js";
 
+// cron jobs
+import "./utils/cron/verificarEstadoEquipos.js";
+
 // RUTAS PARA LA API
 import reactivoRoutes from "./routes/reactivoRoutes.js";
-import equipoRoutes from "./routes/equipoRoutes.js";
+import equipoRoutes from "./routes/equipos/equipoRoutes.js";
+import reservaRoutes from "./routes/equipos/reservaRoutes.js";
+import mantenimientoRoutes from "./routes/equipos/mantenimientoRoutes.js";
 
 const app = express();
 app.use(express.json());
@@ -16,6 +21,8 @@ conectarDB();
 
 app.use("/api/reactivos", reactivoRoutes);
 app.use("/api/equipos", equipoRoutes);
+app.use("/api/equipos/reserva", reservaRoutes);
+app.use("/api/equipos/mantenimiento", mantenimientoRoutes);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
