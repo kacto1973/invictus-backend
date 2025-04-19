654db27c65e992124b7872b695520134906000f3
Pedro Lopez <peanlolo@hotmail.com>
2025-04-19 11:51:48 -0700

    - Solucionando error con CORS

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import conectarDB from './config/db.js';
+import cors from 'cors';
 
 // RUTAS PARA LA API
 import reactivoRoutes from './routes/reactivoRoutes.js';
@@ -12,6 +13,21 @@ app.use(express.json());
 dotenv.config();
 
 conectarDB();
+
+const dominiosPermitidos = ['http://localhost:5173']
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (dominiosPermitidos.indexOf(origin) !== -1 || !origin) {
+            // El Origen del Request esta permitido
+            callback(null, true);
+        } else {
+            callback(new Error('No permitido por CORS'));
+        }
+    },
+};
+
+app.use(cors(corsOptions));
 
 app.use("/api/reactivos", reactivoRoutes);
 
