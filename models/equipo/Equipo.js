079e99f464b740f7a29e2eb963b0db515dc33a43
D3fussionC0ld <diego.quijada03@gmail.com>
2025-04-23 03:47:26 -0700

    Correcciones

--- models/equipo/Equipo.js
+++ models/equipo/Equipo.js
@@ -2,26 +2,27 @@ import mongoose from "mongoose";
 
 const equipoSchema = new mongoose.Schema({
     nombre: {
-      type: String,
-      required: true
+        type: String,
+        required: true,
     },
     descripcion: {
-      type: String,
-      required: true
+        type: String,
+        required: true,
     },
     urlImagen: {
-      type: String,
-      required: true
+        type: String,
+        required: true,
     },
     requiereMantenimiento: {
-      type: Boolean,
-      required: true
+        type: Boolean,
+        required: true,
     },
     status: {
-      type: String,
-      required: true
-    }
+        type: String,
+        enum: ["Liberado", "En mantenimiento", "En reserva", "Eliminado"],
+        required: true,
+    },
 });
 
-const Equipo = mongoose.model('Equipo', equipoSchema);
+const Equipo = mongoose.model("Equipo", equipoSchema);
 export default Equipo;
