cf3a1c89a1c1c9395d581b8022397b7bcd2cb36e
jessica <jessymontano@hotmail.com>
2025-05-05 16:08:38 -0700

    modificar modelos para usar el nombre generado por defecto

--- models/equipo/Reserva.js
+++ models/equipo/Reserva.js
@@ -1,33 +1,30 @@
 import mongoose from "mongoose";
 
-const reservaSchema = new mongoose.Schema(
-  {
-    idEquipo: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Equipo",
-      required: true,
-    },
-    persona: {
-      type: String,
-      required: true,
-    },
-    fechaInicio: {
-      type: Date,
-      default: Date.now,
-      required: true,
-    },
-    fechaFin: {
-      type: Date,
-      required: true,
-    },
-    status: {
-      type: Boolean,
-      default: true,
-      required: true,
-    },
+const reservaSchema = new mongoose.Schema({
+  idEquipo: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Equipo",
+    required: true,
   },
-  { collection: "Reserva" }
-);
+  persona: {
+    type: String,
+    required: true,
+  },
+  fechaInicio: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+  fechaFin: {
+    type: Date,
+    required: true,
+  },
+  status: {
+    type: Boolean,
+    default: true,
+    required: true,
+  },
+});
 
 const Reserva = mongoose.model("Reserva", reservaSchema);
 export default Reserva;
